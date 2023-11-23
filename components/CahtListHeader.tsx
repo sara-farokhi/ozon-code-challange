237e33e6cc140447e80c884f54f835109bacb619
sara-farokhi <sarafarokhi.sf@gmail.com>
2023-11-24 01:45:51 +0330

    add ChatSearchBar component

--- components/CahtListHeader.tsx
+++ components/CahtListHeader.tsx
@@ -1,17 +1,11 @@
 import React from "react";
+import ChatSearchBar from "./ChatSearchBar";
 
 const CahtListHeader = () => {
   return (
     <div className="header-container">
       <div className="chat-search-container">
-        <div className="chat-search-input-container">
-          <i className="search-avatar">J</i>
-          <input
-            className="chat-search-input"
-            placeholder="Search by name, number..."
-          />
-          <i className="search-icon" />
-        </div>
+        <ChatSearchBar />
       </div>
     </div>
   );
