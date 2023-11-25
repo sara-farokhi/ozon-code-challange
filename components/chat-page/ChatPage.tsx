8ca0a2bf1a892b31aa95bbecbf3ce0e05e1009f9
sara-farokhi <sarafarokhi.sf@gmail.com>
2023-11-26 01:11:07 +0330

    add Messages LoadMore to ChatPage component

--- components/chat-page/ChatPage.tsx
+++ components/chat-page/ChatPage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { chats } from "@/types";
+import Messages from "./Messages";
+import { LoadMoreMessages } from "./LoadMoreChats";
 
-const ChatPage = () => {
-  return <div>ChatPage</div>;
+const ChatPage = ({ chats }: chats) => {
+  return (
+    <>
+      <LoadMoreMessages />
+      <Messages chats={chats} />
+    </>
+  );
 };
 
 export default ChatPage;
