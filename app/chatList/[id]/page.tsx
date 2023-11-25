d820aa5d23ba9fbc18cc9fcac3244c82ca77e6fd
sara-farokhi <sarafarokhi.sf@gmail.com>
2023-11-26 01:02:32 +0330

    add fetch chats function to chat page

--- app/chatList/[id]/page.tsx
+++ app/chatList/[id]/page.tsx
@@ -1,10 +1,16 @@
 import ChatPageHeader from "@/components/chat-page/ChatPageHeader";
+import { fetchChats } from "@/actions/fetch-messages";
 import React from "react";
+import ChatPage from "@/components/chat-page/ChatPage";
+import AddMessage from "@/components/chat-page/AddMessage";
 
-const Chat = () => {
+const Chat = async () => {
+  const chats = await fetchChats();
   return (
     <div>
       <ChatPageHeader />
+      <ChatPage chats={chats} />
+      <AddMessage />
     </div>
   );
 };
