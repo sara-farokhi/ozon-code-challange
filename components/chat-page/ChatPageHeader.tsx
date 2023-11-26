6ab24777d882c89b2f586011c765938f6ce6880e
sara-farokhi <sarafarokhi.sf@gmail.com>
2023-11-26 13:03:06 +0330

    fix ChatHeaderComponent righSide icons

--- components/chat-page/ChatPageHeader.tsx
+++ components/chat-page/ChatPageHeader.tsx
@@ -12,8 +12,12 @@ const ChatPageHeader = ({ chats }: chats) => {
     <div className="header-container">
       <div className="chat-search-container">
         <div className="chat-page-header-details">
-          <div className="chat-page-header-info"></div>
-          <div className="chat-page-header-icons"></div>
+          <div className="chat-page-header-info">i</div>
+          <div className="chat-page-header-icons">
+            <i className="call-icon"></i>
+            <i className="video-icon"></i>
+            <i className="mobile-hamburger-menu-icon"></i>
+          </div>
         </div>
       </div>
     </div>
