0f4a696e7bc0636fd30579aa851f56293034222b
sara-farokhi <sarafarokhi.sf@gmail.com>
2023-11-26 13:40:18 +0330

    fix chat page header info left side dtails

--- components/chat-page/ChatPageHeader.tsx
+++ components/chat-page/ChatPageHeader.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { chats } from "@/types";
 import { useParams } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 const ChatPageHeader = ({ chats }: chats) => {
   const params = useParams();
@@ -12,7 +14,21 @@ const ChatPageHeader = ({ chats }: chats) => {
     <div className="header-container">
       <div className="chat-search-container">
         <div className="chat-page-header-details">
-          <div className="chat-page-header-info">i</div>
+          <div className="chat-page-header-info">
+            <Link href="/chatList">
+              <i className="arrow-back-outline-icon"></i>
+            </Link>
+            <Image
+              src={chatInfo!.thumbnailUrl}
+              alt={chatInfo!.title}
+              style={{ borderRadius: "50%", marginRight: "8px" }}
+              width={30}
+              height={30}
+            />
+            <span className="chat-page-header-info-title">
+              {chatInfo!.title.slice(0, 5)}
+            </span>
+          </div>
           <div className="chat-page-header-icons">
             <i className="call-icon"></i>
             <i className="video-icon"></i>
